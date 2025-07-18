521b365d7df422326b79bf5652f2d049b751ca76
marcelprizm <60249216+marcelprizm@users.noreply.github.com>
2025-07-18 17:24:33 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise ValueError("Falta el BOT_TOKEN. Asegúrate de configurarlo en Render.")
 
-# Comando /start con mensaje de bienvenida
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "👋 ¡Bienvenido/a al ecosistema Concilium!\n\nEstamos construyendo el futuro de la descentralización. Pronto recibirás más información."
     )
 
-# Inicializar la app
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-
 if __name__ == "__main__":
-    print("Bot Concilium iniciado en Render...")  # Línea nueva para forzar commit
+    print("Bot Concilium iniciado en Render...")
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
     app.run_polling()
